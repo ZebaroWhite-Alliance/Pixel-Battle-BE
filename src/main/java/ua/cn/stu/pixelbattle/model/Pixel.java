b4aa3154294a86ae4b6b2ef82605bf53ab848597
Dmitro Itsko <dima.itsko.ua@gmail.com>
2025-11-04 15:32:08 +0200

    fix: add @JsonProperty to Pixel for JSON compatibility

--- src/main/java/ua/cn/stu/pixelbattle/model/Pixel.java
+++ src/main/java/ua/cn/stu/pixelbattle/model/Pixel.java
@@ -1,5 +1,6 @@
 package ua.cn.stu.pixelbattle.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +15,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Pixel {
+
+  @JsonProperty("x")
   private int coordinateX;
+
+  @JsonProperty("y")
   private int coordinateY;
+
   private String color;
   private String username;
 }
